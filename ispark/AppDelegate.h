ff398eae8f97e8ae05c7f5a621b879122d196ee5
Natanel Partouche <natanelpartouche@MacBook-Pro-de-Natanel-Partouche.local>
2012-02-15 17:47:05 +0100

    Initial Commit

---
+++ ispark/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  ispark
+//
+//  Created by Natanel Partouche on 15/02/12.
+//  Copyright (c) 2012 ECE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ ispark/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ispark
+//
+//  Created by Natanel Partouche on 15/02/12.
+//  Copyright (c) 2012 ECE. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
